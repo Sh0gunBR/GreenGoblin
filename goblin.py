e3c6bccba87d880f61b83e78ce3732503ffa2617
Sh0gunBR <121455129+Sh0gunBR@users.noreply.github.com>
2023-01-11 12:29:08 -0300

    Update goblin.py

--- goblin.py
+++ goblin.py
@@ -47,13 +47,3 @@ data = {
 		'creation_date': datetime.datetime(1997, 9, 15, 0, 0)
 	}
 print(data)
-whois.whois(input(target))
-data = {
-
-		'expiration_date': datetime.datetime(2020, 9, 14, 0, 0),
-		'last_updated': datetime.datetime(2011, 7, 20, 0, 0),
-		'registrar': 'MARKMONITOR INC.',
-		'name': target,
-		'creation_date': datetime.datetime(1997, 9, 15, 0, 0)
-	}
-print(data)
